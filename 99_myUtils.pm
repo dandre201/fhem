3c724cd090903c7e8dad6d567f82adfbc7177123
dandre201 <darand-8@student.ltu.se>
2019-04-03 13:00:35 +0300

    Add files via upload

---
+++ 99_myUtils.pm
@@ -0,0 +1,20 @@
+##############################################
+# $Id: myUtilsTemplate.pm 7570 2015-01-14 18:31:44Z rudolfkoenig $
+#
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+sub
+myUtils_Initialize($$)
+{
+  my ($hash) = @_;
+}
+
+# Enter you functions below _this_ line.
+
+
+1;
